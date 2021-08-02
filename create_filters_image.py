3469a25727d39818771e0697cee04e716ab3ed49
ikano <u7105226@anu.edu.au>
2021-08-02 11:34:15 +1000

    First commit

---
+++ create_filters_image.py
@@ -0,0 +1,51 @@
+from mpdaf.obj import Cube
+import matplotlib.pyplot as plt
+from astropy.table import Table
+import numpy as np
+import numpy.ma as ma
+
+def filter_im(image_name: str, band: str = None, plot_single: bool = False):
+    '''
+    This function creates broadband images and then saves the data from each filter as .npy files
+    '''
+    # loading in data cube
+    file_name = "C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/data_cubes/"+image_name+".fits"
+    obj1 = Cube(file_name)
+    
+    # getting images of different bands
+    im_g = obj1.get_band_image('SDSS_g')
+    im_r = obj1.get_band_image('SDSS_r')
+    im_i = obj1.get_band_image('SDSS_i')
+    im_z = obj1.get_band_image('SDSS_z')
+    
+    # saving the data out of each band as numpy arrays
+    save_path = 'C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/python files/data/'+image_name
+    
+    np.save(save_path+'/SDSS_g_band/SDSS_g_band_of_'+image_name+'.npy',ma.getdata(im_g))
+    np.save(save_path+'/SDSS_r_band/SDSS_r_band_of_'+image_name+'.npy',ma.getdata(im_r))
+    np.save(save_path+'/SDSS_i_band/SDSS_i_band_of_'+image_name+'.npy',ma.getdata(im_i))
+    np.save(save_path+'/SDSS_z_band/SDSS_z_band_of_'+image_name+'.npy',ma.getdata(im_z))
+    
+    # plotting each
+    fig, axs = plt.subplots(2, 2, figsize=(15, 7))
+    
+    im_g.plot(ax=axs[0,0], title='SDSS_g',zscale = True)
+    im_r.plot(ax=axs[0,1], title='SDSS_r',zscale = True)
+    im_i.plot(ax=axs[1,0], title='SDSS_i',zscale = True)
+    im_z.plot(ax=axs[1,1], title='SDSS_z',zscale = True)
+    
+    plt.savefig(save_path+'/broadband_im.png')
+    plt.show()
+    
+    if plot_single == True:
+        if band not in ['g','r','i','z']:
+            print('Cannot create single image, must specify band g,r,i or z')
+            return
+        
+        # to plot single image
+        # globals() converts what is in [] to be a variable
+        # eg. globals()['im_'+'g'] == im_g
+        plot_im = globals()['im_'+band]
+        
+        plot_im.plot(title='SDSS_'+band,zscale = True, cmap='gray')
+        plt.savefig(save_path+'/SDSS_'+band+'_band/filter_image.png')
---
+++ fit_isophote.py
@@ -0,0 +1,118 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from photutils.aperture import EllipticalAperture
+from photutils.isophote import EllipseGeometry
+from photutils.isophote import Ellipse
+from photutils.isophote import build_ellipse_model
+from photutils.isophote import EllipseSample, Isophote, IsophoteList
+from astropy.io import fits
+
+def fit_isophote(image_name: str, band: str, procedure: str = None, vmin = -3, vmax = 3):
+    '''
+    This function fits isophotes to an image that has been passed through an SDSS filter
+    
+    Inputs:
+    
+        
+    Example:
+        fit_isophote('MAGPI1201','i',procedure='photom')
+        
+    '''
+    
+    #TODO add a check to see if band and procedure strings are valid
+    
+    # path to band image
+    file_name = "C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/python files/data/"+image_name+"/SDSS_"+band+"_band/SDSS_"+band+"_band_of_"+image_name+".npy"
+    
+    data = np.load(file_name)
+    
+    
+    # This code needs to run regardless and doesn't take long so can run each time
+    # following tutorials from https://photutils.readthedocs.io/en/stable/isophote.html and https://mpdaf.readthedocs.io/en/latest/cube.html
+    
+    # create initial guess ellipse
+    geometry = EllipseGeometry(x0=200, y0=200, sma=40, eps=0.25,pa=30*np.pi/180.)
+    
+    # calculating and plotting the initial ellipse guess
+    aper = EllipticalAperture((geometry.x0, geometry.y0), geometry.sma,geometry.sma*(1 - geometry.eps),geometry.pa)
+    plt.imshow(data,origin = 'lower',vmin=vmin,vmax=vmax)
+    aper.plot(color='red')
+    plt.title('Isophote initial guess')
+    plt.savefig("C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/python files/data/"+image_name+"/SDSS_"+band+"_band/Isophote_initial_guess.png")
+    # calculating isophotes from that initial guesss
+    ellipse = Ellipse(data, geometry=geometry)
+    isolist = ellipse.fit_image()
+    
+    # can then print numerous isophotes for different semi-major axes
+    model_image = build_ellipse_model(data.shape, isolist)
+    residual = data - model_image
+    
+    plt.figure()
+    plt.imshow(data, origin = 'lower',vmin=vmin,vmax=vmax)
+    
+    # will plot the ellipses for given sma values
+    smas = np.linspace(10,50,5)
+    
+    # also plot the largest ellipse
+    smas = np.append(smas,max(isolist.sma))
+    
+    print('Plotting isophotes with sma of: \n',smas)
+    
+    # actually plotting the ellipses
+    for sma in smas:
+        iso = isolist.get_closest(sma)
+        x, y, = iso.sampled_coordinates()
+        plt.plot(x,y, color='white')
+    plt.title('Resulting isophote model image')
+    plt.savefig("C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/python files/data/"+image_name+"/SDSS_"+band+"_band/Modelled_isophotes.png")
+    
+    plt.figure()
+    plt.imshow(model_image, origin = 'lower',vmin=vmin,vmax=vmax)
+    plt.title('Modelled isophotes')
+    plt.savefig("C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/python files/data/"+image_name+"/SDSS_"+band+"_band/model_image.png")
+    
+    plt.figure()
+    plt.imshow(residual, origin = 'lower',vmin=vmin,vmax=vmax)
+    plt.title('The noise remove by the modelling process')
+    plt.savefig("C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/python files/data/"+image_name+"/SDSS_"+band+"_band/noise_from_process.png")
+    
+    # This code takes a while and may not be needed every time
+    if procedure == 'photom':
+        # from Step 7 at https://github.com/astropy/photutils-datasets/blob/main/notebooks/isophote/isophote_example4.ipynb
+        
+        with fits.open("C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/data_cubes/"+image_name+".fits") as hdu:
+            cube = hdu[1].data
+        
+        # choosing a few wavelength slices to model first
+        # qfits indexes from 1, so slice number 2000 in pyfits is 1999 in python
+        wavelength_indexes = [499,1999,2999]
+        
+        for wavelength_index in wavelength_indexes:
+            isolist_narrow_ = []
+            
+            # TODO didn't work if just had [:], maybe because g.sma = 0 for this?
+            for iso in isolist[1:]:
+                # get the EllipseGeometry of each modelled ellipse
+                g = iso.sample.geometry
+                
+                # using the model image (just one wavelength slice for testing) so can subtract source
+                sample = EllipseSample(cube[wavelength_index], g.sma, geometry=g, integrmode='median', sclip=3.0, nclip=3)
+                sample.update()
+                
+                iso_ = Isophote(sample, 0, True, 0)
+                isolist_narrow_.append(iso_)
+                
+                # constructing the isophote list from the result
+                isolist_narrow = IsophoteList(isolist_narrow_)
+                
+            # then plot the intensity profile
+            # only use isolist.intens[1:] as removed first elem of isolist_narrow
+            # TODO make it so don't have to do this??
+            plt.figure()
+            errors = isolist_narrow.intens/isolist.intens[1:] * np.sqrt((isolist_narrow.int_err / isolist_narrow.intens)**2 + (isolist.int_err[1:] / isolist.intens[1:])**2)
+            plt.errorbar(isolist_narrow.sma**0.25, (isolist_narrow.intens / isolist.intens[1:]),yerr=errors, fmt='o', markersize=4)
+            plt.title("Intensity Profile of wavelength slice "+str(wavelength_index+1))
+            plt.xlabel('sma**1/4')
+            plt.ylabel('Ratio')
+            # add wavelength+1 so that wavelength of file is that as shown in qfitsview
+            plt.savefig("C:/Users/isaac/Documents/Uni 2021/Sem 2/ASTR3005/data/python files/data/"+image_name+"/SDSS_"+band+"_band/Intensity_profile_wavelength_slice_"+str(wavelength_index+1)+".png")
