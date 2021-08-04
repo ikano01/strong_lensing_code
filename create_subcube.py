d61b7ac2b55e89a9530f829f09996c7a9a339885
ikano <u7105226@anu.edu.au>
2021-08-04 17:55:57 +1000

    Uploaded wrong subcube creation file last time

--- create_subcube.py
+++ create_subcube.py
@@ -1,38 +1,64 @@
 from mpdaf.obj import Cube
-from astropy.io import fits
-import numpy as np
-import matplotlib.pyplot as plt
+from mpdaf.obj import WCS
+import astropy.units as u
 
-def create_subcube(image_name:str, centre:(int,int),radius:int):
+def create_subcube(image_name:str, centre:(float,float), length_coord:(float,float), subtype:str = 'cube', length_axis:str = 'x'):
     '''
     This function will create and save a new subcube.
     
-    Example:
-        create_subcube('MAGPI1201', (197,197), 25)
+    MPDAF images are stored in python arrays that are indexed in [y,x] axis order
+    
+    Examples:
+        create_subcube('MAGPI1201', (197,197), (256,197),'y')
+    or
+        create_subcube('MAGPI1501', (210,192), (263,192),'y')
     '''
-    # TODO Need to fix this file!!! Does not work
-
-
+    
     # load in full cube
     file_name = "C:/Users/isaac/Documents/Uni_2021/Sem_2/ASTR3005/data/data_cubes/"+image_name+".fits"
+    full_cube = Cube(file_name)
     
-    with fits.open(file_name) as hdul:
-        hdul.info()
-        
-        # opening DATA
-        data = hdul[1].data
-        print('\n Loaded data of shape',np.shape(data))
+    # Due qfitsview to indexing from 1, need to subtract 1 from the pixel count
+    centre[0] -= 1
+    centre[1] -= 1
+    length_coord[0] -= 1
+    length_coord[1] -= 1
     
-    # define coordinates to cut along
-    x_coord_subset = [centre[0]-radius,centre[0]+radius]
-    y_coord_subset = [centre[1]-radius,centre[1]+radius]
+    # Instancing the WCS class, which deals with world coordinates
+    wcs = WCS(full_cube.get_wcs_header())
+    # converting centre into degrees
+    centre = wcs.pix2sky(centre)[0]
+    print('Centre in (ra,dec):',centre)
     
-    # create subcube
-    data = np.array(data)
-    subcube = data[:,y_coord_subset,x_coord_subset]
+    # converting length coordinate to degrees
+    # TODO MIGHT NEED TO INPUT BOX LENGTH AS COORDINATE SO HAVE Y AND X AND THEN ONLY INPUT ONE OF THEM INTO THE SUBCUBE FUNCTION
+    # had to index [0] because wcs.pix2sky output the coordinates in a second list, eg. [[y,x]]
+    length_coord = wcs.pix2sky(length_coord)[0]
+    
+    # getting the length in degrees by subtracting y-degree (or x) of centre with y-degree (or x) of length_coord
+    if length_axis == 'x':
+        length = abs(centre[1]-length_coord[1])
+    elif length_axis == 'y':
+        length = abs(centre[0]-length_coord[0])
+    print('Cube side length in degrees:',length)
+    
+    # converting length to arcseconds
+    # .value just gives the number without the unit
+    length = ((length*u.deg).to(u.arcsec)).value
+    print('Cube side length in arcseconds:',length)
+    
+    
+    if subtype == 'cube':
+        # creating cubic subcube
+        subcube = full_cube.subcube(center = (centre[0],centre[1]),size = length)
+    elif subtype == 'circle':
+        # creating circle aperature subcube
+        subcube = full_cube.subcube_circle_aperture(centre,radius=length)
+    else:
+        print('Must specify sub_type of either cube or circle')
+        return
     
     # saving subcube
     save_name = "C:/Users/isaac/Documents/Uni_2021/Sem_2/ASTR3005/data/data_cubes/"+image_name+"_subcube.fits"
-
-
-create_subcube('MAGPI1201', (197,197), 25)
+    
+    subcube.write(save_name)
