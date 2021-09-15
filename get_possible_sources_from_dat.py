7a7aeb91e459c7c9a6f05cb62e18ed3fcbf11666
ikano <u7105226@anu.edu.au>
2021-09-15 23:52:42 +1000

    Fixed z index being wrong by 1 index

--- get_possible_sources_from_dat.py
+++ get_possible_sources_from_dat.py
@@ -4,6 +4,9 @@ import ctypes
 file_name = 'C:/Users/isaac/Documents/Uni_2021/Sem_2/ASTR3005/data/\
 python files/data/MAGPI1501_subcube/SDSS_i_band/\
 SDSS_i_residual_image_MAGPI1501_subcube_MAGPI1501_i_subcube_LSDCat_IKA_class - Copy.dat'
+file_name = 'C:/Users/isaac/Documents/Uni_2021/Sem_2/ASTR3005/data/\
+python files/data/MAGPI1201_subcube/SDSS_r_band/\
+SDSS_r_residual_image_MAGPI1201_subcube_MAGPI1201_r_subcube_LSDCat_IKA_class - Copy.dat'
 
 ### ONLY USE WITH COPY OF FILE JUST IN CASE ###
 
@@ -19,9 +22,9 @@ for line_num,possible_source in enumerate(file_lines):
         # assuming max pixel num is < 100
         # x elem is at [24:26]
         # y elem is at [33:35]
-        # z elem is at [41:45]
+        # z elem is at [40:45]
         
-        answer = ctypes.windll.user32.MessageBoxW(0, 'x: '+str(possible_source[24:26])+' y: '+str(possible_source[33:35])+' z: '+str(possible_source[41:45])+"Is this a galaxy \n Yes to make type 1, No for 2 and Cancel for 3", "Type "+str(possible_source[-2])+" Galaxy", 3)
+        answer = ctypes.windll.user32.MessageBoxW(0, 'x: '+str(possible_source[24:26])+' y: '+str(possible_source[33:35])+' z: '+str(possible_source[40:44])+" Is this a galaxy \n Yes to make type 1, No for 2 and Cancel for 3", "Type "+str(possible_source[-2])+" Galaxy", 3)
         # on final line of dat file, it doesn't have a \n so need to do differently
         if line_num != len(file_lines)-1:
             # if click yes (returns 6) change classification to yes a galaxy
