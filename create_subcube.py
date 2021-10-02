dffd91261f4bda48b65fdaee3a94d61fddb2deb2
ikano <u7105226@anu.edu.au>
2021-10-03 01:24:40 +1000

    Got masking working!

--- create_subcube.py
+++ create_subcube.py
@@ -14,10 +14,10 @@ def create_subcube(image_name:str, centre:[int,int], length_coord:[int,int], sub
         length_coord:[int,int] is the coordinate of the distance to cut to, either vertically or horizontally from the centre_coord depending if length_axis is 'x' or 'y'
         length_axis:str can be either 'y' if the length_coord is vertical from the centre_coord or 'x' if the length_coord is horizontal from the centre_coord
         mask_cube:bool if True adds a mask to the subcube
-        mask_centres:list is a list of lists which each correspond to the [y,x] centre of a mask
-        mask_extents:list is a list of lists which each correspond to the [y,x] extents of the mask
+        mask_centres:list is a list of lists which each correspond to the [y,x] centre of a mask to be placed on the full cube
+        mask_extents:list is a list of lists which each correspond to the [y,x] extents of the mask to be placed on the full cube
         larger_subcube:bool if True, this will save the subcube with _larger added to the filename
-        pixels_larger:int is the number of pixels wider and higher the new subcube should be
+        pixels_larger:int is the number of pixels wider and higher on each side the new subcube should be
     
     MPDAF images are stored in python arrays that are indexed in [y,x] axis order
     
@@ -29,6 +29,8 @@ def create_subcube(image_name:str, centre:[int,int], length_coord:[int,int], sub
         create_subcube('MAGPI1501', [210,192], [293,192],subtype = 'cube',length_axis='y', larger_subcube = True, pixels_larger = 30)
     or
         create_subcube('MAGPI1203', [197,197], [260,197],subtype = 'cube',length_axis='y')
+    or a mask example
+        create_subcube('MAGPI1203', [197,197], [290,197],length_axis='y',mask_cube = True, mask_centres = [[161,198]],mask_extents = [[9,9]])
     or
         create_subcube('MAGPI1508', [195,200], [235,195],subtype = 'cube',length_axis='y')
     '''
@@ -49,28 +51,29 @@ def create_subcube(image_name:str, centre:[int,int], length_coord:[int,int], sub
     # Instancing the WCS class, which deals with world coordinates
     wcs = WCS(full_cube.get_wcs_header())
     # converting centre into degrees
-    centre = wcs.pix2sky(centre)[0]
-    print('Centre in (ra,dec):',centre)
+    centre_deg = wcs.pix2sky(centre)[0]
+    print('Centre in (ra,dec):',centre_deg)
     
     
     # converting length coordinate to degrees
-    # TODO MIGHT NEED TO INPUT BOX LENGTH AS COORDINATE SO HAVE Y AND X AND THEN ONLY INPUT ONE OF THEM INTO THE SUBCUBE FUNCTION
     # had to index [0] because wcs.pix2sky output the coordinates in a second list, eg. [[y,x]]
-    length_coord = wcs.pix2sky(length_coord)[0]
+    length_coord_deg = wcs.pix2sky(length_coord)[0]
     
-    # getting the length in degrees by subtracting y-degree (or x) of centre with y-degree (or x) of length_coord
+    # getting the length in degrees by subtracting y-degree (or x) of centre_deg with y-degree (or x) of length_coord_deg
     if length_axis == 'x':
         if larger_subcube:
             # if larger_subcube, want to make length pixels_larger bigger
-            length = abs(centre[1]-length_coord[1])+pixels_larger
+            length = abs(centre_deg[1]-length_coord_deg[1])+2*pixels_larger
         else:
-            length = abs(centre[1]-length_coord[1])
+            length = abs(centre_deg[1]-length_coord_deg[1])
+        
     elif length_axis == 'y':
         # if larger_subcube, want to make length pixels_larger bigger
         if larger_subcube:
-            length = abs(centre[0]-length_coord[0])+pixels_larger
+            length = abs(centre_deg[0]-length_coord_deg[0])+2*pixels_larger
         else:
-            length = abs(centre[0]-length_coord[0])
+            length = abs(centre_deg[0]-length_coord_deg[0])
+        
     else:
         raise SyntaxError('Must specify whether to use the y or x value of the length coordinate [y,x]')
     print('Cube side length in degrees:',length)
@@ -83,29 +86,41 @@ def create_subcube(image_name:str, centre:[int,int], length_coord:[int,int], sub
     
     if subtype == 'cube':
         # creating cubic subcube
-        subcube = full_cube.subcube(center = (centre[0],centre[1]),size = length)
+        subcube = full_cube.subcube(center = (centre_deg[0],centre_deg[1]),size = length)
     elif subtype == 'circle':
         # creating circle aperature subcube
-        subcube = full_cube.subcube_circle_aperture(centre,radius=length)
+        subcube = full_cube.subcube_circle_aperture(centre_deg,radius=length)
     else:
         raise SyntaxError('Must specify sub_type of either cube or circle')
         return    
     
     # masking the cube
-    ### TODO won't actually be able to see the subcube when set these so need to be able to put in centre and extents etc. to be for the full subcube
-    ### maybe could translate it by number of pixels away from the subcube centre???
     if mask_cube:
         for i,mask_centre in enumerate(mask_centres):
+            # first need to convert the mask_centre coordinates from the full cube to the subcube
+
+            # all of the pixels in the subcube shifted coordinates when cropped from the subcube.
+            # as the subcube is a square centered on the centre pixel and centre pixel in full cube coordinates is known
+            # the pixel shift is the difference between the centre full cube coord and its subcube coordinate which is 
+            # at the centre of the cube, (so this is len(subcube_slice)/2)
+            # centre is the centre coords in the full cube coordinates
+            pixel_shift = [centre[1] - len(subcube.data[0])/2,centre[0] - len(subcube.data[0])/2]
+            
+            # the mask centre in the subcube is then the centre coordinate in the full cube coordinates minus the pixel_shift
+            mask_centre[0] = mask_centre[0] - pixel_shift[0]
+            mask_centre[1] = mask_centre[1] - pixel_shift[1]
+            
             # mask extents actually have to be input as (x,y), though I thought this would be confusing to input as the rest is (y,x) so need to change it here
             mask_extent = [mask_extents[i][1],mask_extents[i][0]]
             
             if larger_subcube:
-                # to fit the mask to the correct position in larger subcube, needs to be shifted by pixels_larger/2
-                mask_centre = [mask_centre[0]+pixels_larger/2,mask_centre[1]+pixels_larger/2]
+                # to fit the mask to the correct position in larger subcube, needs to be shifted by pixels_larger 
+                # as the origin position shifts by 30 pixels in both x and y
+                mask_centre = [mask_centre[0]+pixels_larger,mask_centre[1]+pixels_larger]
             
             # I set the rotation to 0, but if need it can implement it by lettting user change 0 to something else
             # unit_center and unit_radius are set to None so the centre and extent values have units of pixels
-            subcube.mask_ellipse(mask_centre,mask_extents,0,unit_center = None, unit_radius = None)
+            subcube.mask_ellipse(mask_centre,mask_extent,0,unit_center = None, unit_radius = None)
         pass
     
     
@@ -117,4 +132,4 @@ def create_subcube(image_name:str, centre:[int,int], length_coord:[int,int], sub
     
     subcube.sum(axis=0).plot()
     plt.show()
-    subcube.write(save_name,savemask='nan')
+    #subcube.write(save_name,savemask='nan')
