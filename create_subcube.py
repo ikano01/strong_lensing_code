31981a5aa3eb6f126c36e920d3cc9360b253e3b7
ikano <u7105226@anu.edu.au>
2021-10-04 20:22:16 +1100

    Made it so dont need to convert to degrees when make subcube and also just need to put in the distance extent for subcube rather than coordinate

--- create_subcube.py
+++ create_subcube.py
@@ -3,7 +3,7 @@ from mpdaf.obj import WCS
 import astropy.units as u
 import matplotlib.pyplot as plt
 
-def create_subcube(image_name:str, centre:[int,int], length:[int,int], subtype:str = 'cube', length_axis:str = 'x', 
+def create_subcube(image_name:str, centre:[int,int], length:[int,int], subtype:str = 'cube', 
     mask_cube:bool = False, larger_subcube:bool = False, pixels_larger:int = 0, mask_centres:list = [], mask_extents:list = []):
     '''
     This function will create and save a new subcube.
@@ -11,8 +11,7 @@ def create_subcube(image_name:str, centre:[int,int], length:[int,int], subtype:s
     Inputs:
         cube_name:str is the name of the downloaded full cube
         centre:[int,int] is the coordinate in terms of [y,x] that will be the centre of the subcube
-        length_coord:[int,int] is the coordinate of the distance to cut to, either vertically or horizontally from the centre_coord depending if length_axis is 'x' or 'y'
-        length_axis:str can be either 'y' if the length_coord is vertical from the centre_coord or 'x' if the length_coord is horizontal from the centre_coord
+        length:int is the radius in pixels to cut to (so the whole cube has side lengths of 2*length)
         mask_cube:bool if True adds a mask to the subcube
         mask_centres:list is a list of lists which each correspond to the [y,x] centre of a mask to be placed on the full cube
         mask_extents:list is a list of lists which each correspond to the [y,x] extents of the mask to be placed on the full cube
@@ -22,17 +21,17 @@ def create_subcube(image_name:str, centre:[int,int], length:[int,int], subtype:s
     MPDAF images are stored in python arrays that are indexed in [y,x] axis order
     
     Examples:
-        create_subcube('MAGPI1201', [197,197], [256,197],subtype = 'cube',length_axis='y')
+        create_subcube('MAGPI1201', [197,197], 59,subtype = 'cube')
     or
-        create_subcube('MAGPI1501', [210,192], [263,192],subtype = 'cube',length_axis='y')
+        create_subcube('MAGPI1501', [210,192], 53,subtype = 'cube')
     or to create larger subcube cut
-        create_subcube('MAGPI1501', [210,192], [293,192],subtype = 'cube',length_axis='y', larger_subcube = True, pixels_larger = 30)
+        create_subcube('MAGPI1501', [210,192], 83,subtype = 'cube', larger_subcube = True, pixels_larger = 30)
     or
-        create_subcube('MAGPI1203', [197,197], [260,197],subtype = 'cube',length_axis='y')
+        create_subcube('MAGPI1203', [197,197], 63,subtype = 'cube')
     or a mask example
-        create_subcube('MAGPI1203', [197,197], [290,197],length_axis='y',mask_cube = True, mask_centres = [[161,198]],mask_extents = [[9,9]])
+        create_subcube('MAGPI1203', [197,197], 93,mask_cube = True, mask_centres = [[161,198]],mask_extents = [[9,9]])
     or
-        create_subcube('MAGPI1508', [195,200], [235,195],subtype = 'cube',length_axis='y')
+        create_subcube('MAGPI1508', [195,200], 40,subtype = 'cube')
     '''
     
     # load in full cube
@@ -44,64 +43,25 @@ def create_subcube(image_name:str, centre:[int,int], length:[int,int], subtype:s
     
     # initialising centre_coord and length arrays
     centre_coord = [0,0]
-    length_coord = [0,0]
+    length_extent = [0,0]
     
     # Due qfitsview to indexing from 1, need to subtract 1 from the pixel count
     centre_coord[0] = centre[0] - 1
     centre_coord[1] = centre[1] - 1
-    length_coord[0] = length[0] - 1
-    length_coord[1] = length[1] - 1
-    
-    # Instancing the WCS class, which deals with world coordinates
-    wcs = WCS(full_cube.get_wcs_header())
-    # converting centre into degrees
-    centre_deg = wcs.pix2sky(centre_coord)[0]
-    print('Centre in (ra,dec):',centre_deg)
+    length_extent = length - 1
     
     # if larger_subcube, want to make length pixels_larger bigger
-    # TODO fix, I don't think this works? Because what if pixel is below the object
-    # then the += would mean it would be less distance from the centre?
     if larger_subcube:
-        if length_axis == 'x':
-            # want to make length_coord[1] pixels_larger further from the centre
-            if length_coord[1] > centre_coord[1]:
-                length_coord[1] += pixels_larger
-            else:
-                length_coord[1] -= pixels_larger
-        else:
-            # want to make length_coord[0] pixels_larger further from the centre
-            if length_coord[0] > centre_coord[0]:
-                length_coord[0] += pixels_larger
-            else:
-                length_coord[0] -= pixels_larger
-    
-    # converting length coordinate to degrees
-    # had to index [0] because wcs.pix2sky output the coordinates in a second list, eg. [[y,x]]
-    length_coord_deg = wcs.pix2sky(length_coord)[0]
-    
-    # getting the length in degrees by subtracting y-degree (or x) of centre_deg with y-degree (or x) of length_coord_deg
-    if length_axis == 'x':
-        length = abs(centre_deg[1]-length_coord_deg[1])
-        
-    elif length_axis == 'y':
-        length = abs(centre_deg[0]-length_coord_deg[0])
-        
-    else:
-        raise SyntaxError('Must specify whether to use the y or x value of the length coordinate [y,x]')
-    print('Cube side length in degrees:',length)
-    
-    # converting length to arcseconds
-    # .value just gives the number without the unit
-    length = ((length*u.deg).to(u.arcsec)).value
-    print('Cube side length in arcseconds:',length)
-    
+        # want to make length_extent[1] pixels_larger further from the centre
+        length_extent += pixels_larger
     
     if subtype == 'cube':
         # creating cubic subcube
-        subcube = full_cube.subcube(center = (centre_deg[0],centre_deg[1]),size = length)
+        # input requires centre to be in degrees and size to be in arcsec
+        subcube = full_cube.subcube(center = centre_coord,size = length_extent, unit_center = None, unit_size = None)
     elif subtype == 'circle':
         # creating circle aperature subcube
-        subcube = full_cube.subcube_circle_aperture(centre_deg,radius=length)
+        subcube = full_cube.subcube_circle_aperture(centre_deg,radius=length_arcsec)
     else:
         raise SyntaxError('Must specify sub_type of either cube or circle')
         return    
