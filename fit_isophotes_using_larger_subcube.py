5d95c223c7fb4cba132e5a9640d9b8e0e0a8ee2f
ikano <u7105226@anu.edu.au>
2021-10-04 20:22:27 +1100

    Made it so dont need to convert to degrees when make subcube and also just need to put in the distance extent for subcube rather than coordinate

--- fit_isophotes_using_larger_subcube.py
+++ fit_isophotes_using_larger_subcube.py
@@ -6,14 +6,13 @@ import numpy as np
 
 ### code to fit isophote to larger subcube and then copy onto a smaller cube ###
 
-def run(cube_name,centre,length,length_axis, mask_cube = False, mask_centres = [], mask_extents = [],
+def run(cube_name,centre,length, mask_cube = False, mask_centres = [], mask_extents = [],
     pixels_larger = 30,init_ellipse = [30, 30, 15, 0.15,30],check_init_ellipse = True,band = 'g',SN_threshold = 10):
     '''
     Inputs:
         cube_name:str is the name of the downloaded full cube
         centre:[int,int] is the coordinate of the centre of the lens galaxy where coordinates are expressed [y,x]
-        length:[int,int] is the coordinate of the distance to cut to, either vertically or horizontally from the centre_coord depending if length_axis is 'x' or 'y'
-        length_axis:str can be either 'y' if the length_coord is vertical from the centre_coord or 'x' if the length_coord is horizontal from the centre_coord
+        length:int is the radius in pixels to cut to (so the whole cube has side lengths of 2*length)
         mask_cube:bool if True adds a mask to the subcube
         mask_centres:list is a list of lists which each correspond to the [y,x] centre of a mask to be placed on the full cube
         mask_extents:list is a list of lists which each correspond to the [y,x] extents of the mask to be placed on the full cube - Can measure number of pixels using Qfitsview by pressing p
@@ -26,20 +25,24 @@ def run(cube_name,centre,length,length_axis, mask_cube = False, mask_centres = [
         SN_threshold: int is the signal to noise threshold that counts as a detection within the LSDCat processing steps
         
         eg.
-        run('MAGPI1201',[197,197], [256,197],length_axis='y', 30,[30, 30, 15, 0.15,30],check_init_ellipse = True,band='g', SN_threshold = 10)
+        run('MAGPI1201',[197,197], 59, 30,[30, 30, 15, 0.15,30],check_init_ellipse = True,band='g', SN_threshold = 10)
         or
-        run('MAGPI1203',[196,196], [196,261],'x', 20,[45, 45, 15, 0.1,0],check_init_ellipse = True,band='g', SN_threshold = 10)
+        run('MAGPI1203',[196,196], 65, 20,[45, 45, 15, 0.1,0],check_init_ellipse = True,band='g', SN_threshold = 10)
         or for the masked version,
-        run(cube_name='MAGPI1203',centre=[196,196], length=[196,261],length_axis='x',mask_cube=True,mask_centres=[[153,197],[194,213],[238,191],[194,163],[153,195],[227,219],[205,245],[170,233],[
+        run(cube_name='MAGPI1203',centre=[196,196], length=65,mask_cube=True,mask_centres=[[153,197],[194,213],[238,191],[194,163],[153,195],[227,219],[205,245],[170,233],[
             170,249],[176,262]],mask_extents=[[24,35],[5,5],[17,25],[4,3],[11,7],[6,16],[10,7],[8,7],[7,7],[7,9]], pixels_larger=20,init_ellipse=[45, 45, 15, 0.1,0], 
+            check_init_ellipse = True,band='g', SN_threshold = 10)
+        or
+        run(cube_name='MAGPI1205',centre=[191,199], length=38,mask_cube=True,mask_centres=[[157,197],[211,226],[223,177],[238,200]],
+            mask_extents=[[13,16],[4,4],[7,7],[5,5]], pixels_larger=20,init_ellipse=[25, 25, 20, 0.1,0], 
             check_init_ellipse = True,band='g', SN_threshold = 10)
     '''
     # if the cube should be masked maskmask_cube == True
     # create subcube
-    create_subcube(cube_name, centre, length,subtype = 'cube',length_axis=length_axis, larger_subcube = False, pixels_larger = pixels_larger, mask_cube = mask_cube, mask_centres = mask_centres,mask_extents = mask_extents)
+    create_subcube(cube_name, centre, length,subtype = 'cube', larger_subcube = False, pixels_larger = pixels_larger, mask_cube = mask_cube, mask_centres = mask_centres,mask_extents = mask_extents)
     
     # create larger subcube
-    create_subcube(cube_name, centre, length,subtype = 'cube',length_axis=length_axis, larger_subcube = True, pixels_larger = pixels_larger, mask_cube = mask_cube, mask_centres = mask_centres,mask_extents = mask_extents)
+    create_subcube(cube_name, centre, length,subtype = 'cube', larger_subcube = True, pixels_larger = pixels_larger, mask_cube = mask_cube, mask_centres = mask_centres,mask_extents = mask_extents)
 
     # create filters image of both cubes
     filter_im(cube_name+'_subcube')
